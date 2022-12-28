4a850d65230d8ca313cf1e1861dc0c78d162d417
lerrybe <lerry597@gmail.com>
2022-12-29 02:29:21 +0900

    feat: Dim 컴포넌트 (dimmed 처리를 위함) 구현

---
+++ src/components/drawer/dim/dim.styled.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  top: 0;
+  left: 0;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.401);
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.5);
+  z-index: 999;
+`;
