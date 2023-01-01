2a2c042c1b3c401700177b9cf83ac88e510a1e27
lerrybe <lerry597@gmail.com>
2023-01-01 19:57:44 +0900

    chore: current - transaction-info

--- src/pages/profile/container/transaction-info/index.tsx
+++ src/pages/profile/container/transaction-info/index.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import TxTitle from '../../components/tx-title';
+
 import * as S from './transaction-info.styled';
+import { ReactComponent as TxInfoIcon } from '../../../../assets/txinfo-icon.svg';
 
 const TxInfo = () => {
-  return <S.Wrapper>TxInfo</S.Wrapper>;
+  const [username, setUsername] = useState<string>('');
+
+  useEffect(() => {
+    setUsername('lerrybe');
+  }, []);
+
+  return (
+    <S.Wrapper>
+      <S.Header>
+        <TxInfoIcon />
+        <S.Title>Transaction Info</S.Title>
+      </S.Header>
+
+      <S.TempWrapper>
+        <TxTitle text="와플온도" />
+
+      </S.TempWrapper>
+
+      <S.LocationWrapper>
+        <TxTitle text={`${username}의 동네`} />
+        
+      </S.LocationWrapper>
+    </S.Wrapper>
+  );
 };
 
 export default TxInfo;
--- src/pages/profile/container/transaction-info/transaction-info.styled.tsx
+++ src/pages/profile/container/transaction-info/transaction-info.styled.tsx
@@ -1,6 +1,54 @@
 import styled from 'styled-components';
+import { MD_SIZE } from '../../../../constant/breakpoint';
 
 export const Wrapper = styled.div`
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 330px;
   height: auto;
+  border-left: 2px solid #eaebee;
+  padding-left: 15px;
+
+  @media ${MD_SIZE} {
+    padding-left: 0;
+    border-left: none;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 22px;
+  margin-bottom: 34px;
+
+  @media ${MD_SIZE} {
+    margin-bottom: 24px;
+    padding-bottom: 22px;
+    border-bottom: 2px solid #eaebee;
+    box-sizing: border-box;
+  }
+`;
+
+export const Title = styled.span`
+  font-weight: 500;
+  font-size: 14px;
+  color: #acacac;
+  margin-left: 8px;
+`;
+
+export const TempWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  margin-bottom: 34px;
+`;
+
+export const LocationWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
 `;
