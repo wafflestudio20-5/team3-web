393f3086f17434524d4d870bdab2bfaf3e346136
lerrybe <lerry597@gmail.com>
2023-01-01 18:30:53 +0900

    feat: 프로필 페이지 UI 구현

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,17 +3,25 @@ import { useNavigate } from 'react-router-dom';
 
 import Gnb from '../../components/gnb';
 import Header from './components/header';
+import UserInfo from './container/user-info';
+import TxInfo from './container/transaction-info';
 import NavigationButton from './components/navigation-button';
 
 import * as S from './profile.styled';
-
-import daangn from '../../assets/daangn.svg'
+import buyIcon from '../../assets/buy-icon.svg';
+import lifeIcon from '../../assets/life-icon.svg';
+import likeIcon from '../../assets/like-icon.svg';
+import sellIcon from '../../assets/sell-icon.svg';
+import reviewIcon from '../../assets/review-icon.svg';
+import mannerCommentIcon from '../../assets/manner-comment-icon.svg';
 
 const ProfilePage = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState<string | null>(null);
 
-  // TODO: API 호출, getUsername, getUserId
+  // TODO: 토큰 가져오기
+  // TODO: API 호출, GET /users/me
+  // TODO: 요청 실패시 에러 처리 (프로필 페이지 접근 X)
   useEffect(() => {
     setUsername('lerrybe');
   }, []);
@@ -21,10 +29,47 @@ const ProfilePage = () => {
   return (
     <S.Wrapper>
       <Gnb />
-
       <S.ContentWrapper>
+        {/* TODO: My chats 이동, 혹은 채팅 띄우기 */}
         <Header username={username} handleClick={() => navigate('/')} />
-        <NavigationButton img={daangn} text={'판매내역'} handleClick={() => navigate('/')} />
+        <S.InfoWrapper>
+          <UserInfo />
+          <TxInfo />
+        </S.InfoWrapper>
+
+        {/* TODO: 적절한 페이지로 이동 */}
+        <S.NavigationWrapper>
+          <NavigationButton
+            img={sellIcon}
+            text="판매내역"
+            handleClick={() => navigate('/')}
+          />
+          <NavigationButton
+            img={buyIcon}
+            text="구매내역"
+            handleClick={() => navigate('/')}
+          />
+          <NavigationButton
+            img={likeIcon}
+            text="찜한 상품"
+            handleClick={() => navigate('/')}
+          />
+          <NavigationButton
+            img={reviewIcon}
+            text="거래후기"
+            handleClick={() => navigate('/')}
+          />
+          <NavigationButton
+            img={mannerCommentIcon}
+            text="매너평가"
+            handleClick={() => navigate('/')}
+          />
+          <NavigationButton
+            img={lifeIcon}
+            text="동네생활"
+            handleClick={() => navigate('/')}
+          />
+        </S.NavigationWrapper>
       </S.ContentWrapper>
     </S.Wrapper>
   );
--- src/pages/profile/profile.styled.tsx
+++ src/pages/profile/profile.styled.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { MD_SIZE, SM_to_MD_SIZE } from '../../constant/breakpoint';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -16,5 +17,38 @@ export const ContentWrapper = styled.div`
   max-width: 712px;
   min-width: 360px;
   height: auto;
-  margin-top: 35px;
+  padding: 30px 16px 0 16px;
+
+  @media ${SM_to_MD_SIZE} {
+    padding: 35px 20px;
+  }
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  width: 100%;
+
+  @media ${MD_SIZE} {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const NavigationWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+  row-gap: 10px;
+  column-gap: 10px;
+  width: 100%;
+  margin-top: 48px;
+  padding-top: 36px;
+  border-top: 2px solid #f5f5f5;
+
+  @media ${MD_SIZE} {
+    width: 336px;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 10px;
+    column-gap: 2px;
+  }
 `;
