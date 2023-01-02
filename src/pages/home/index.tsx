7213f39ca15c09dfdb4710ecef9bddde899dc545
lerrybe <lerry597@gmail.com>
2023-01-03 08:24:03 +0900

    refactor: 테스트 콘솔 삭제

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,13 +4,6 @@ import Gnb from '../../components/gnb';
 import { Wrapper } from './home.styled';
 
 const HomePage = () => {
-  // TODO: 테스트용 fetch
-  // useEffect(() => {
-  //   fetch('/users/me')
-  //     .then(response => response.json())
-  //     .then(data => console.log(data));
-  // }, []);
-
   return (
     <Wrapper>
       <Gnb />
