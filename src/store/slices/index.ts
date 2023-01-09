376a42435438ef16fbce421155060f3b7bc05eb1
lerrybe <lerry597@gmail.com>
2023-01-09 18:29:17 +0900

    feat: 세션 slice 작성 예시

--- src/store/slices/index.ts
+++ src/store/slices/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import usersSlice from './usersSlice';
+import sessionSlice from './sessionSlice';
 
 const rootReducer = combineReducers({
   users: usersSlice,
+  session: sessionSlice,
 });
 
 export default rootReducer;
