cfd85138078afebb26f22e4a4fe2663956896fb3
roddywhite <roddywhite1111@gmail.com>
2023-01-10 23:52:48 +0900

    feat: 마켓 라우팅 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import HomePage from '../pages/home';
 import ErrorPage from '../pages/error';
 import LoginPage from '../pages/login';
 import KaKaoLogin from '../pages/login/kakao';
-import SignUpPage from '../pages/signup';
 import ProfilePage from '../pages/profile';
+import MarketPage from '../pages/market';
 
 // DESC: 라우팅 관리를 위한 EntryRoute
 function EntryRoute() {
@@ -16,8 +16,9 @@ function EntryRoute() {
         <Route path="/*" element={<ErrorPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/login/kakao" element={<KaKaoLogin />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        
         <Route path="/profile/:id" element={<ProfilePage />} />
+        <Route path="/market" element={<MarketPage />} />
       </Routes>
     </BrowserRouter>
   );
