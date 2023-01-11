0c542da38802be1a94ad4381178bfb1759a028ad
lerrybe <lerry597@gmail.com>
2023-01-12 03:35:54 +0900

    chore: moment 라이브러리 설치

--- src/pages/trade-post/index.tsx
+++ src/pages/trade-post/index.tsx
@@ -7,11 +7,12 @@ import * as S from './trade-post.styled';
 const TradePostPage = () => {
   // const accessToken = useAppSelector(state => state.accessToken);
   // TODO: 없을 때 이전 페이지로
-  
+
   return (
     <S.Wrapper>
       <Gnb />
       <S.ContentWrapper>
+        {/* TODO: 캐러샐 슬라이더 구현 */}
         <S.SampleImg
           src="https://dnvefa72aowie.cloudfront.net/origin/article/202008/2F22EE23018C3A490E6C3596917934B9B2C80A2958862C4BE49A54BE0AFA6953.jpg?q=95&s=1440x1440&t=inside"
           alt="img"
