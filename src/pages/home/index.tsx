c0420b2ee9b8f2216b82d27f1a43d8cd7b68cf89
lerrybe <lerry597@gmail.com>
2023-01-12 02:11:55 +0900

    chore: 홈화면에 중고거래 상세페이지 link 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-// import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Gnb from '../../components/gnb';
 import { Wrapper } from './home.styled';
@@ -10,6 +9,7 @@ const HomePage = () => {
       <a>홈페이지입니다</a>
       <Link to="/login">로그인</Link>
       <Link to="/signup">회원가입</Link>
+      <Link to="/tradepost/1">중고거래 상세페이지</Link>
     </Wrapper>
   );
 };
