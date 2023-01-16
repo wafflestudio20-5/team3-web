3cc0b0698f27b9f5c97710dad6834137c10e8259
lerrybe <lerry597@gmail.com>
2023-01-17 03:46:35 +0900

    feat: 스피너, 로딩중 컴포넌트

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,7 +1,13 @@
-import { Wrapper } from './loading.styled';
+import { LoadingAnnounce, LoadingGif, Wrapper } from './loading.styled';
+import loadingImg from '../../assets/loading-block.gif';
 
 const Loading = () => {
-  return <Wrapper>Loading...</Wrapper>;
+  return (
+    <Wrapper>
+      <LoadingGif src={loadingImg} />
+      <LoadingAnnounce>로딩 중 입니다..</LoadingAnnounce>
+    </Wrapper>
+  );
 };
 
 export default Loading;
