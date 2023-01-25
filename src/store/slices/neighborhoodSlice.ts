4c2ba6346559632792b7ce455c7e1e1e55145219
Changmin Yoo <fluentmin@gmail.com>
2023-01-26 01:20:49 +0900

    feat: neighborhoodSlice 추가

---
+++ src/store/slices/neighborhoodSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { comment } from '../../types/neighborhood';
+
+const initialState: Array<comment> = [] as comment[];
+
+export const commentsSlice = createSlice({
+  name: 'comments',
+  initialState,
+  reducers: {
+    setComments: (state, { payload }) => {
+      console.log(payload);
+      return (state = payload);
+    },
+    addComment: (state, { payload }) => {
+      return [
+        ...state,
+        {
+          comment: payload.comment,
+          commentId: payload.commentId,
+          commenter: payload.commenter,
+          createdAt: payload.createdAt,
+          isHidden: payload.isHidden,
+          isOwner: payload.isOwner,
+          modifiedAt: payload.modifiedAt,
+        },
+      ];
+    },
+  },
+});
+
+export const { setComments, addComment } = commentsSlice.actions;
+export default commentsSlice.reducer;
