e5ecbe300d7a03f73063e91722f8ef3efbe5e678
roddywhite <roddywhite1111@gmail.com>
2023-01-25 15:34:36 +0900

    feat: 다른 유저 판매목록 스타일 수정

--- src/pages/sell-history-others/components/shortcut/shortcut.styled.tsx
+++ src/pages/sell-history-others/components/shortcut/shortcut.styled.tsx
@@ -22,6 +22,8 @@ export const Container = styled.div`
     width: 100%;
     height: 180px;
     border-top: 0.5px solid black;
+    border-bottom: 0.5px solid black;
+    border-collapse: collapse;
     border-radius: 0;
     gap: 4px;
   }
