1f019a9cc57111d51068c7948089ac487a1219d7
lerrybe <lerry597@gmail.com>
2023-01-30 19:39:34 +0900

    feat: 내 동네 범위 설정하기 api 제외

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Gnb from '../../components/gnb';
@@ -20,6 +20,9 @@ import lifeIcon from '../../assets/life-icon.svg';
 import likeIcon from '../../assets/like-icon.svg';
 import sellIcon from '../../assets/sell-icon.svg';
 import reviewIcon from '../../assets/review-icon.svg';
+import scopeWide from '../../assets/scope-wide.png';
+import scopeNarrow from '../../assets/scope-narrow.png';
+import scopeNormal from '../../assets/scope-normal.png';
 import mannerCommentIcon from '../../assets/manner-comment-icon.svg';
 
 const ProfilePage = () => {
@@ -28,8 +31,30 @@ const ProfilePage = () => {
   const { sessionLoading } = useAuth();
   const [modalOpen, setModalOpen] = useState(false);
   const { me } = useAppSelector(state => state.users);
-  const { myChats, unreadTotalCount } = useAppSelector(state => state.chat);
+  const [openAreaModal, setOpenAreaModal] = useState(false);
   const { accessToken } = useAppSelector(state => state.session);
+  const { myChats, unreadTotalCount } = useAppSelector(state => state.chat);
+
+  const [rangeValue, setRangeValue] = useState(0);
+  const [rangeImg, setRangeImg] = useState(scopeNarrow);
+  const [rangeDistance, setRangeDistance] = useState(35);
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setRangeValue(Number(e.target.value));
+  }, []);
+
+  useEffect(() => {
+    if (rangeValue === 0) {
+      setRangeImg(scopeNarrow);
+      setRangeDistance(35);
+    } else if (rangeValue === 1) {
+      setRangeImg(scopeNormal);
+      setRangeDistance(200);
+    } else if (rangeValue === 2) {
+      setRangeImg(scopeWide);
+      setRangeDistance(400);
+    }
+  }, [rangeValue]);
 
   const [edit, setEdit] = useState({
     img: false,
@@ -93,6 +118,7 @@ const ProfilePage = () => {
               edit={edit}
               isLoading={sessionLoading}
               setEdit={setEdit}
+              setOpenAreaModal={setOpenAreaModal}
             />
           </S.InfoWrapper>
 
@@ -172,6 +198,33 @@ const ProfilePage = () => {
               </S.ModalInnerWrapper>
             )}
           </>
+        </ModalWrapper>
+      )}
+
+      {openAreaModal && (
+        <ModalWrapper handleClose={() => setOpenAreaModal(false)}>
+          <S.DisplayWrapper>
+            <S.HeaderWrapper>
+              <S.RangeTitle>동네 범위 선택</S.RangeTitle>
+              <S.SubmitRange onClick={() => alert('변경')}>변경하기</S.SubmitRange>
+            </S.HeaderWrapper>
+            <S.RangeAnnounce>
+              선택한 범위의 게시글만 볼 수 있어요.
+            </S.RangeAnnounce>
+            <S.RangeAnnounceTitle>{`내 주소 근처 ${rangeDistance}km`}</S.RangeAnnounceTitle>
+            <S.Range
+              type="range"
+              min="true"
+              max="2"
+              value={rangeValue}
+              onChange={handleChange}
+            />
+            <S.RangeDesc>
+              <S.Desc>가까운 동네</S.Desc>
+              <S.Desc>먼 동네</S.Desc>
+            </S.RangeDesc>
+            <S.ScopeImg src={rangeImg} alt="scope" />
+          </S.DisplayWrapper>
         </ModalWrapper>
       )}
     </S.OuterWrapper>
