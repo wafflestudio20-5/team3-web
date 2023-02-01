3399fb8958b59903ee32e9c01ca48ea9453d9a2e
Yeji Kim <lerry597@gmail.com>
2023-02-02 01:24:09 +0900

    feat: landing 이미지 슬라이더 (캐러샐) (#104)
    * feat: section0
    
    * feat: 이미지 파일 업로드
    
    * feat: 랜딩 캐러샐 구현

--- src/pages/home/section1/section1.styled.tsx
+++ src/pages/home/section1/section1.styled.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Carousel from 'react-material-ui-carousel';
 import { MD_SIZE } from '../../../constant/breakpoint';
 
 export const Wrapper = styled.div`
@@ -27,30 +28,125 @@ export const Title = styled.h1`
 `;
 
 export const CategoryWrapper = styled.div`
-  display: grid;
-  justify-content: center;
-  grid-template-columns: 320px 320px;
-  grid-template-rows: 178px 178px;
   width: 100%;
-  height: auto;
-  margin-top: 70px;
-  gap: 20px;
+  height: 500px;
+  margin-top: 60px;
 
   @media ${MD_SIZE} {
-    grid-template-columns: 320px;
-    margin-top: 50px;
+    height: 650px;
   }
 `;
 
-export const Category = styled.div`
+export const InnerWrapper = styled(Carousel)`
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+`;
+
+export const Background1 = styled.div`
+  width: 100%;
+  height: 467px;
+  background: #e27c43;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media ${MD_SIZE} {
+    height: 580px;
+  }
+`;
+
+export const Background2 = styled(Background1)`
+  background: #ffe7d1;
+`;
+
+export const FlexWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: auto;
+  gap: 40px;
+
+  @media ${MD_SIZE} {
+    flex-direction: column;
+  }
+`;
+
+export const SliderImg = styled.img`
+  width: 371px;
+  height: auto;
+`;
+
+export const InfoWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
   justify-content: center;
-  align-items: center;
-  width: 320px;
-  height: 178px;
-  font-family: 'KyoboHand';
-  background: #fff;
-  border-radius: 10px;
-  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.1);
+`;
+
+export const AppLogoA = styled.img`
+  width: 47px;
+  height: 47px;
+  margin-bottom: 30px;
+`;
+
+export const PhraseA1 = styled.img`
+  width: 286px;
+  height: auto;
+  margin-bottom: 22px;
+`;
+
+export const PhraseA2 = styled.div`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 21px;
+  color: #ffffff;
+  margin-bottom: 35px;
+`;
+
+export const PhraseB1 = styled.img`
+  width: 259px;
+  height: auto;
+  margin-bottom: 19px;
+  margin-top: 50px;
+`;
+
+export const PhraseB2 = styled.div`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 21px;
+  color: #ff7e36;
+  margin-bottom: 33px;
+`;
+
+export const AppLogoB = styled.img`
+  width: 47px;
+  height: 47px;
+`;
+
+export const PhraseC1 = styled.img`
+  width: 277px;
+  height: auto;
+  margin-bottom: 22px;
+`;
+
+export const PhraseC2 = styled.div`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 21px;
+  color: #ffffff;
+  white-space: pre-line;
+  margin-bottom: 35px;
+`;
+
+export const LogoWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const AppLogoC = styled.img`
+  width: 47px;
+  height: 47px;
 `;
