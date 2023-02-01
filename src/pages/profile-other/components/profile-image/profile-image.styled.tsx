cd971f816101dd0ad140cc23ee7dbef2592a4fa7
lerrybe <lerry597@gmail.com>
2023-02-01 19:07:59 +0900

    fix: 남의 프로필 정리

--- src/pages/profile-other/components/profile-image/profile-image.styled.tsx
+++ src/pages/profile-other/components/profile-image/profile-image.styled.tsx
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export const PositionWrapper = styled.div`
   position: relative;
-  width: 155px;
-  height: 155px;
-  margin-bottom: 24px;
+  width: 170px;
+  height: 170px;
+  margin-bottom: 30px;
+  margin-top: 10px;
 `;
 
 export const ProflieImg = styled.img`
