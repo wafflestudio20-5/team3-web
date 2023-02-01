6ea30292fd2963e758724dd42f380a9d124271a5
lerrybe <lerry597@gmail.com>
2023-02-01 09:45:01 +0900

    fiX: useAuth 사용한 곳들 정리

--- src/pages/trade-post/index.tsx
+++ src/pages/trade-post/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import Gnb from '../../components/gnb';
 import Spinner from '../../components/spinner';
 import TradeInfo from './container/trade-info';
@@ -5,13 +7,20 @@ import OtherTrades from './container/other-trades';
 import ContentFooter from '../../components/content-footer';
 
 import { useAuth } from '../../hooks/useAuth';
+import { normalToast } from '../../utils/basic-toast-modal';
 
 import * as S from './trade-post.styled';
 
 const TradePostPage = () => {
-  const { sessionLoading } = useAuth();
+  const navigate = useNavigate();
+  const { sessionLoading, isAuthed } = useAuth();
 
-  if (sessionLoading) {
+  if (!sessionLoading && !isAuthed) {
+    navigate('/login');
+    normalToast('로그인이 필요합니다.');
+  }
+
+  if (sessionLoading || !isAuthed) {
     return <Spinner />;
   }
 
