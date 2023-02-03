35c74f59e85acf2dd6eed54b6a0a7fe07c7677b1
Yeji Kim <lerry597@gmail.com>
2023-02-03 17:29:12 +0900

    fix: 정규표현식 크로스브라우징 이슈 해결 (#139)

--- src/utils/price.ts
+++ src/utils/price.ts
@@ -1,6 +1,11 @@
 // DESC: 타입의 경우 무조건 string, String 타입 캐스팅이 필요하다면 호출부에서 캐스팅
-export const toStringNumberWithComma = (input?: string) =>
-  input?.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
+export const toStringNumberWithComma = (input?: string) => {
+  if (!input) return '';
+  return input.replace(
+    /(\..*)$|(\d)(?=(\d{3})+(?!\d))/g,
+    (digit, fract) => fract || digit + ',',
+  );
+};
 
 // DESC: 타입의 경우 무조건 string, String 타입 캐스팅이 필요하다면 호출부에서 캐스팅
 export const toNumberWithoutComma = (input?: string) =>
